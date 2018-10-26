e55c1337d1c8304c46509f1ec5ce20b1c6ccb5dd
YujiToshinaga <y.toshinaga@gmail.com>
2018-10-26 20:33:54 +0900

    UARTの割込み設定のバグを修正

--- Loader/bcm283x.h
+++ Loader/bcm283x.h
@@ -103,8 +103,8 @@
 #define AUX_ENABLES_SPI2_EN_BIT (0x1 << 2)
 #define MU_IO_DATA_SHIFT        0
 #define MU_IO_DATA_MASK         (0xff << 0)
-#define MU_IER_EN_TX_INT_BIT    (0x1 << 1)
-#define MU_IER_EN_RX_INT_BIT    (0x1 << 0)
+#define MU_IER_EN_RX_INT_BIT    (0x1 << 1)
+#define MU_IER_EN_TX_INT_BIT    (0x1 << 0)
 #define MU_IIR_FIFO_CLR         (0x3 << 1)
 #define MU_LCR_DS_8BIT          (0x3 << 0)
 #define MU_LSR_TX_IDLE_BIT      (0x1 << 6)
